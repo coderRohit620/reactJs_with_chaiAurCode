d1f9c392a5d3e6bcb694b48fdc7c91507582091b
coderRohit620 <rohitravi62023@gmail.com>
2026-08-19 18:46:24 +0530

    Add Chai component and update App to render it

--- 01basicreact/src/App.js
+++ 01basicreact/src/App.js
@@ -1,7 +1,10 @@
-
+import Chai from "./Chai.js"
 function App() {
   return (
-    <h1>Chai aur Code | Rohit Ravi</h1>
+    <>
+      <h1>Chai aur Code | Rohit Ravi</h1>
+      <Chai/>
+    </>
   );
 }
 
---
+++ 01basicreact/src/Chai.js
@@ -0,0 +1,8 @@
+
+function Chai(){
+    return (
+        <h3>Hello Rohit . Chai is ready</h3>
+    )
+}
+
+export default Chai
