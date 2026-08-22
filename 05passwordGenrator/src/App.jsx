8696a862cd477717162ab8c430612a80fa617d66
coderRohit620 <rohitravi62023@gmail.com>
2026-08-22 16:17:11 +0530

    fix: remove unnecessary cursor pointer style from Copy button

--- 05passwordGenrator/src/App.jsx
+++ 05passwordGenrator/src/App.jsx
@@ -50,7 +50,7 @@ function App() {
           />
           <button 
           onClick={copyPasswordToClipboard}
-          className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0 hover:bg-sky-700 cursor-pointer'>Copy</button>
+          className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0 hover:bg-sky-700 '>Copy</button>
         </div>
 
         <div className='flex text-sm gap-x-2'>
